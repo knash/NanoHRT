fb29fa78eefa260ff409fe05974037662225d523
Kevin <knash201@gmail.com>
2019-01-23 07:23:05 -0500

    ImageMD and new training

--- plugins/ImageProducer.cc
+++ plugins/ImageProducer.cc
@@ -46,6 +46,7 @@ class ImageProducer : public edm::stream::EDProducer<edm::GlobalCache<ImageTFCac
     const edm::EDGetTokenT<edm::View<pat::Jet>> sj_;
     std::string sdmcoll_;
     edm::FileInPath pb_path_;
+    std::string extex_;
 
 };
 
@@ -54,7 +55,8 @@ ImageProducer::ImageProducer(const edm::ParameterSet& iConfig,  const ImageTFCac
  tfsession_(nullptr)
 , src_(consumes<edm::View<pat::Jet>>(iConfig.getParameter<edm::InputTag>("src")))
 , sj_(consumes<edm::View<pat::Jet>>(iConfig.getParameter<edm::InputTag>("sj")))
-, sdmcoll_((iConfig.getParameter<std::string>("sdmcoll")))
+, sdmcoll_(iConfig.getParameter<std::string>("sdmcoll"))
+, extex_(iConfig.getParameter<std::string>("extex"))
 {
   
   produces<pat::JetCollection>();
@@ -367,7 +369,7 @@ void ImageProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   jindex=0;
   for (const auto &jet : *jets){
     pat::Jet newJet(jet);
-    newJet.addUserFloat("Image:top", itopdisc[jindex]);
+    newJet.addUserFloat("Image"+extex_+":top", itopdisc[jindex]);
     outputs->push_back(newJet);
     jindex+=1;
   }
